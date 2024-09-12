295fae361422c4f7a65daf39ee24c6a4cfda61ae
MASOUMA2023 <ahmadimasooma12@gmail.com>
2024-09-12 19:42:14 -0400

    some progress on project

--- js/index.js
+++ js/index.js
@@ -1,17 +1,85 @@
-const userName = 'MASOUMA2023';
-fetch (`https://api.github.com/users/${userName}/repos`)
-.then(response => {
-    if(response.ok){
-        return response.json();
-    } else {
-        throw new Error("failed to fetch data!");
-    }
-    })
-    .then(data => {
-        console.log(data); // Log the response data to the console   
-    })
-    .catch(error => {
-        console.error('Error:', error);   
-    });
 
+const peopleContainer = document.getElementById('people-container');
+const personContainer = document.getElementById('person-container');
+const personHeader = document.getElementById('person-header');
+const personDetails = document.getElementById('person-details');
+const doneButton = document.getElementById('done');
+
+personContainer.hidden = true;
+
+async function fetchPeople(){
+    try{
+        const response = await fetch("https://www.swapi.tech/api/people");
+        if (!response.ok){
+            throw new Error('failed!');
+        }
+let data = await response.json();
+console.log(data);
+const dataLength = data.total_pages;
+console.log('fetch successfull:', dataLength);
+const baseUrl = "https://www.swapi.tech/api/people?page=";
+const urls = [];
+for ( let i = 1; i<= dataLength; i++){
+    urls.push(`${baseUrl}${i}&limit=10`);
+}
+
+ await getAllPages(urls);
+    } catch(err){
+        console.error(err);
+    }
+}
+fetchPeople();
+
+async function getAllPages(urls){
+    try {
+        const promiseList = urls.map(url =>
+            fetch(url)
+                .then(response => response.json())
+                .catch(err => console.error('Failed to fetch:',url, err))
+        );
+    const results = await Promise.all(promiseList);
+        let finalList=[];
+        results.forEach(res => {
+            finalList = finalList.concat(res.results);
+        });
+        console.log(finalList);
+
+        finalList.forEach(person => {
+            const personElt = document.createElement('div');
+            personElt.className ='person';
+
+             const personHeader = document.createElement('h2');
+            personHeader.innerHTML = person.name;
+            personElt.appendChild(personHeader);
+            peopleContainer.appendChild(personElt);
+            personElt.addEventListener('click', async() =>{
+                while (personDetails.firstChild){
+                    personDetails.removeChild(personDetails.firstChild);
+                }
+
+                try {
+                    const personResponse = await fetch(person.url);
+                    const personData = await personResponse.json();
+
+                
+                for (const propKey in personData.result.properties){
+                if ( propKey === 'name')  {
+                    personHeader.innerText = personData.result.properties[propKey];
+                } else{ 
+                    const propItem= document.createElement('li');
+                    propItem.innerText = '${propKey}: ${personData.result.properties[propKey]}';
+                    personDetails.appendChild(propItem);
+                }
+                }          
+        personContainer.hidden= false;
+        window.scrollTo(0,0);
+} catch (err) {
+    console.error('Failed to fetch person details',err);
+}
+            });
+        })
+    } catch(err){
+        console.error("failed tofetch all pages", err);
+    } 
     
+}
